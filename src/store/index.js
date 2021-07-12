1a4e1a767e01e482e6453c2d3d4435036146e53b
Mert Aktaş <mertaktas231@gmail.com>
2021-07-13 02:51:20 +0300

    Changed search api action

--- src/store/index.js
+++ src/store/index.js
@@ -30,12 +30,12 @@ export default createStore({
     },
   },
   actions: {
-    async getSearchData({ commit }, payload) {
-      axios
-        .get(`https://api.themoviedb.org/3/search/multi?query=${payload}&api_key=75019398caf1e5d87c0e4198fc9f17e2`)
-        .then(response => {
-            commit('SET_SEARCH_DATA', response.data.results)
-      })
+    async fetchGetSearchData({ commit }, payload) {
+      MediaService.getSearchData(payload)
+        .then(res => {
+          commit('SET_SEARCH_DATA', res.data.results)
+        })
+        .catch(err => console.log(err)) 
     },
     async fetchPopular({ commit }) {
       MediaService.getPopularMovies()
