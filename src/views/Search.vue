caaec6dd5fd9aea8a5447ce2d16319966a1bb4bc
Mert Aktaş <mertaktas231@gmail.com>
2021-07-15 19:54:47 +0300

    router link search data

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -2,22 +2,24 @@
   <SectionInner />
     <ul v-show="searchData" class="search-section">
       <li :key="data.id" v-for="data in searchData" class="card-search">
-        <img class="image-search" :src="'https://image.tmdb.org/t/p/w500'+ (data.poster_path || data.profile_path)">
-        <div class="content-search">
-          <div class="title-search">
-            <div class="title">
-              {{ data.name || data.title }}
+        <router-link :to="{name: data.known_for_department ? 'actor-show' : 'media-show' , params:{ id: data.id, cat: data.release_date ? 'movie' : data.first_air_date ? 'tv' : 'person' || 'default' } }">
+          <img class="image-search" :src="'https://image.tmdb.org/t/p/w500'+ (data.poster_path || data.profile_path)">
+          <div class="content-search">
+            <div class="title-search">
+              <div class="title">
+                {{ data.name || data.title }}
+              </div>
+              <div class="release_date">
+                {{ data.release_date || data.first_air_date }}
+              </div>
             </div>
-            <div class="release_date">
-              {{ data.release_date || data.first_air_date }}
+            <div class="overview">
+              <p>
+                {{ data.overview }}
+              </p>
             </div>
           </div>
-          <div class="overview">
-            <p>
-              {{ data.overview }}
-            </p>
-          </div>
-        </div>
+        </router-link>
       </li>
     </ul>
 </template>
@@ -57,53 +59,56 @@ export default {
     border: 1px solid rgba( 227,227,227, 1);
     background-color: #fff;
     margin: 10px;
-    .image-search {
-      width: 150px;
-      height: 225px;
-    }
-    .content-search {
-      width: 100%;
-      padding: 10px 15px;
-      box-sizing: border-box;
+    a {
       display: flex;
-      flex-wrap: wrap;
-      align-items: center;
-      .title-search {
+      .image-search {
+        width: 150px;
+        height: 225px;
+      }
+      .content-search {
         width: 100%;
+        padding: 10px 15px;
+        box-sizing: border-box;
         display: flex;
         flex-wrap: wrap;
-        align-items: baseline;
-        overflow: hidden;
-        color: #000;
-        .title {
+        align-items: center;
+        .title-search {
           width: 100%;
-          font-size: 1.2em;
-          line-height: 1.2em;
-          margin-bottom: 0;
+          display: flex;
+          flex-wrap: wrap;
+          align-items: baseline;
+          overflow: hidden;
+          color: #000;
+          .title {
+            width: 100%;
+            font-size: 1.2em;
+            line-height: 1.2em;
+            margin-bottom: 0;
+            display: -webkit-box;
+            -webkit-line-clamp: 1;
+            -webkit-box-orient: vertical;
+            text-overflow: ellipsis;
+            overflow: hidden;
+            margin: 0 0 4px 0;
+            font-weight: 600;
+          }
+          .release_date {
+            white-space: nowrap;
+            color: #999;
+          }
+        }
+        .overview {
+          margin-top: 10px;
+          height: auto;
+          font-size: 1em;
           display: -webkit-box;
-          -webkit-line-clamp: 1;
+          -webkit-line-clamp: 6;
           -webkit-box-orient: vertical;
           text-overflow: ellipsis;
           overflow: hidden;
-          margin: 0 0 4px 0;
-          font-weight: 600;
+          margin: 0;
+          color: #000;
         }
-        .release_date {
-          white-space: nowrap;
-          color: #999;
-        }
-      }
-      .overview {
-        margin-top: 10px;
-        height: auto;
-        font-size: 1em;
-        display: -webkit-box;
-        -webkit-line-clamp: 6;
-        -webkit-box-orient: vertical;
-        text-overflow: ellipsis;
-        overflow: hidden;
-        margin: 0;
-        color: #000;
       }
     }
   }
