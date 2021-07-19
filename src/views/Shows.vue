dcd5338db07293521018defa3759059c0078c09a
Mert Aktaş <mertaktas231@gmail.com>
2021-07-20 01:41:52 +0300

    Shows updated

--- src/views/Shows.vue
+++ src/views/Shows.vue
@@ -1,22 +1,13 @@
 <template>
-<SectionInner/>
-<SectionMain :datas="datas" title="Shows" />
-
+  <SectionInner/>
+  <SectionMain :datas="datas" title="Shows" />
 </template>
 
 <script>
-import axios from 'axios';
 import SectionInner from '@/components/SectionInner.vue'
 import SectionMain from '@/components/SectionMain.vue'
 import { mapActions, mapMutations, mapState } from 'vuex'
-
-
 export default {
-  data() {
-    return {
-      showsdata: null,
-    }
-  },
   name: 'Shows',
   components: {
     SectionInner,
